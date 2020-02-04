b1aa0a014eae028861baa0f0099d622d964d21c3
rajitbanerjee <rajit.banerjee@ucdconnect.ie>
2020-02-04 17:02:38 +0000

    Pool class constructor and resetPool()

---
+++ src/Pool.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Pool {
+    private ArrayList<Tile> pool;
+
+    Pool() {
+        pool = new ArrayList<>();
+        resetPool();
+    }
+
+    public void resetPool() {
+        pool.clear();
+
+        // different types of tiles, "-" represents the blank tile
+        String[] tileTypes = {"-", "E", "AI", "O", "NRT", "LSU",
+                "D", "G", "BCMP", "FHVWY", "K", "JX", "QZ"};
+        // number of tiles of each type
+        int[] numTiles = {2, 12, 9, 8, 6, 4, 4, 3, 2, 2, 1, 1, 1};
+        // points for each type of tile
+        int[] points = {0, 1, 1, 1, 1, 1, 2, 2, 3, 4, 5, 8, 10};
+
+        // add required number of tiles of each type to the pool
+        for (int i = 0; i < tileTypes.length; i++) {
+            for (int j = 0; j < numTiles[i]; j++) {
+                addTiles(tileTypes[i], points[i]);
+            }
+        }
+    }
+}
