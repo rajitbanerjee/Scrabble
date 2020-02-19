762902cecb153b63f4d8691ade37d34ab9d21968
= <katarina.c99@gmail.com>
2020-02-19 10:05:59 +0000

    Initialise isFirstMove to true when board is created

--- src/Board.java
+++ src/Board.java
@@ -18,7 +18,7 @@ public class Board {
      */
     public Board() {
         board = new Square[15][15];
-        isFirstMove = false;
+        isFirstMove = true;
         // set Centre and Normal squares
         for (int i = 0; i < 15; i++) {
             for (int j = 0; j < 15; j++) {
