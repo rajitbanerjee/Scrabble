3dd82920cd763519191070b41298f6bc42527be6
= <katarina.c99@gmail.com>
2020-02-20 00:38:06 +0000

    Changes to testWordPlacementValidity()

--- test/BoardTest.java
+++ test/BoardTest.java
@@ -104,24 +104,98 @@ class BoardTest {
         Pool pool = new Pool();
         Frame frame = new Frame(pool);
         ArrayList<Tile> f = new ArrayList<>();
-        for (char ch : "HEL".toCharArray()) {
+        for (char ch : "HELLO".toCharArray()) {
             f.add(Tile.makeTile(ch));
         }
         frame.setFrame(f);
-        assertFalse(board.isWordPlacementValid('H', 8, 'A', "HELLO", frame));
+
+        //test for isValid()
+        assertTrue(board.isWordPlacementValid('H', 8, 'A', "Hello", frame));
+        assertFalse(board.isWordPlacementValid('R', 7, 'A', "HELLO", frame));
+        assertFalse(board.isWordPlacementValid((char) ('A' - 1), 7, 'A', "HELLO", frame));
+        assertFalse(board.isWordPlacementValid('H', -1, 'A', "Hello", frame));
+        assertFalse(board.isWordPlacementValid('H', 16, 'A', "Hello", frame));
+
+        //test for correct orientation
+        assertTrue(board.isWordPlacementValid('H', 8, 'A', "Hello", frame));
+        assertTrue(board.isWordPlacementValid('H', 8, 'D', "Hello", frame));
+        assertTrue(board.isWordPlacementValid('H', 8, 'a', "Hello", frame));
+        assertTrue(board.isWordPlacementValid('H', 8, 'd', "Hello", frame));
+        assertFalse(board.isWordPlacementValid('H', 8, 'r', "Hello", frame));
+        assertFalse(board.isWordPlacementValid('H', 8, 'q', "Hello", frame));
+
+        //testing for too short a word
+        assertFalse(board.isWordPlacementValid('H', 8, 'A', "H", frame));
+        assertTrue(board.isWordPlacementValid('H', 8, 'A', "He", frame));
+
+        //test for alphabetic word
+        assertFalse(board.isWordPlacementValid('H', 8, 'A', "123", frame));
+        assertFalse(board.isWordPlacementValid('H', 8, 'A', "AB?", frame));
+        assertTrue(board.isWordPlacementValid('H', 8, 'A', "Hello", frame));
+
+        //test for null frame object
+        assertFalse(board.isWordPlacementValid('H', 8, 'A', "123", null));
+
+        //test to check if first move covers the centre square
+        board.reset();
+        assertTrue(board.isWordPlacementValid('E', 8, 'A', "Hello", frame));
+        assertFalse(board.isWordPlacementValid('A', 8, 'A', "Hello", frame));
+
+        //test for isWordJoined()
+        board.setFirstMove(false);
+        board.getBoard()[7][('H' - 'A')].setTile(new Tile('Z', 10));
+        //valid horizontal placements
+        assertTrue(board.isWordPlacementValid('H', 9, 'A', "Hello", frame));
+        assertTrue(board.isWordPlacementValid('H', 7, 'A', "Hello", frame));
+        assertTrue(board.isWordPlacementValid('F', 7, 'A', "Hello", frame));
+        assertTrue(board.isWordPlacementValid('C', 8, 'A', "Hello", frame));
+        assertTrue(board.isWordPlacementValid('I', 8, 'A', "Hello", frame));
+        assertTrue(board.isWordPlacementValid('D', 9, 'A', "Hello", frame));
+        //valid vertical placements
+        assertTrue(board.isWordPlacementValid('H', 9, 'D', "Hello", frame));
+        assertTrue(board.isWordPlacementValid('H', 3, 'D', "Hello", frame));
+        assertTrue(board.isWordPlacementValid('G', 7, 'D', "Hello", frame));
+        assertTrue(board.isWordPlacementValid('G', 4, 'D', "Hello", frame));
+        assertTrue(board.isWordPlacementValid('I', 7, 'D', "Hello", frame));
+        assertTrue(board.isWordPlacementValid('I', 4, 'D', "Hello", frame));
+        //invalid placements
+        assertFalse(board.isWordPlacementValid('H', 12, 'A', "Hello", frame));
+        assertFalse(board.isWordPlacementValid('H', 5, 'A', "Hello", frame));
+        assertFalse(board.isWordPlacementValid('I', 7, 'A', "Hello", frame));
+        assertFalse(board.isWordPlacementValid('I', 9, 'A', "Hello", frame));
+        assertFalse(board.isWordPlacementValid('A', 0, 'D', "Hello", frame));
+        assertFalse(board.isWordPlacementValid('A', 7, 'D', "Hello", frame));
+
+        //test for isOverflowed()
+        board.getBoard()[7][('O' - 'A')].setTile(new Tile('Z', 10));    //place tile at edge of board
+        board.getBoard()[14][('H' - 'A')].setTile(new Tile('Z', 10));    //place tile at edge of board
+        assertFalse(board.isWordPlacementValid('O', 9, 'A', "Hello", frame));
+        assertFalse(board.isWordPlacementValid('N', 6, 'A', "HELLO", frame));
+        assertFalse(board.isWordPlacementValid('I', 15, 'D', "HELLO", frame));
+        assertFalse(board.isWordPlacementValid('G', 12, 'D', "Hello", frame));
+
+        //test for doesWordConflict()
+        board.reset();
+        board.getBoard()[7][('H' - 'A')].setTile(new Tile('Z', 10));
+        board.setFirstMove(false);
+        assertFalse(board.isWordPlacementValid('H', 8, 'A', "Hello", frame));
+        assertFalse(board.isWordPlacementValid('F', 8, 'A', "Hello", frame));
+        assertFalse(board.isWordPlacementValid('H', 8, 'D', "Hello", frame));
+        assertFalse(board.isWordPlacementValid('H', 6, 'D', "Hello", frame));
+
+        //test for frameContainTiles()
+        ArrayList<Tile> f2 = new ArrayList<>();
+        for (char ch : "H-LL".toCharArray()) {
+            f2.add(Tile.makeTile(ch));
+        }
+        frame.setFrame(f2);
+        assertTrue(board.isWordPlacementValid('H', 9, 'A', "He", frame));
+        assertTrue(board.isWordPlacementValid('H', 9, 'A', "lleH", frame));
+        assertFalse(board.isWordPlacementValid('H', 9, 'A', "Hello", frame));
         /*
          * TODO
-         *  1. Test for square out of bounds
-         *  2. Test for invalid orientation
-         *  3. Test for too short a word
-         *  4. Test for alphabetic word
-         *  5. Test for null frame object
-         *  6. Test for board overflow detection
-         *  7. Test for conflict in word placement with existing words on board
-         *  8. Test to check that frame contains required tiles for the word
          *  9. Test to check if the word uses at least one letter from frame
          *  10. Test to check if the first move covers the centre square
-         *  11. Test to check that the placed word connects with another word on board
          */
     }
 
