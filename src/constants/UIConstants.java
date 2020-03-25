757b9c0c0ada3f6fba6128495d5f8bfc6ef5cd82
rajitbanerjee <rajit.banerjee@ucdconnect.ie>
2020-03-25 15:59:22 +0000

    :bug: fix TileView height issue

--- src/constants/UIConstants.java
+++ src/constants/UIConstants.java
@@ -14,8 +14,6 @@ public class UIConstants {
     // Scene height is the board's size as well
     public static final int CMD_INPUT_WIDTH = SCENE_WIDTH - SCENE_HEIGHT - 2;
     public static final double SQUARE_SIZE = SCENE_HEIGHT * 0.0578125;
-    public static final double BOARD_HGAP = SCENE_HEIGHT * 0.005;
-    public static final double BOARD_VGAP = SCENE_HEIGHT * 0.005;
     public static final int DASH_LENGTH = (int) (CMD_INPUT_WIDTH * 0.175);
 
     public enum STATUS_CODE {P1_NAME, P2_NAME, P1_TURN, P2_TURN, GAME_OVER}
