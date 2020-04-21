cc071a5d49eeadaca0deb0718ae5f8a84f766912
= <katarina.c99@gmail.com>
2020-04-21 21:50:01 +0100

    Nested Move class

--- src/DarkMode.java
+++ src/DarkMode.java
@@ -286,6 +286,62 @@ public class DarkMode implements BotAPI {
         }
     }
 
+    // Nested class for moves
+    public static class Move {
+
+        private boolean horizontal; // True if the move is horizontal, false if vertical.
+        private String word; // The full word that will be created.
+        private Coordinate start; // The start location of the first letter.
+        private ArrayList<Integer> newLetters; // Which letters in word are being placed by the player.
+        private ArrayList<Integer> blanks; // Which letters in word are blank tiles.
+        //blanks may not be necessary, we can remove it later if it isn't
+
+        /**
+         * Instantiates move
+         *
+         * @param horizontal True if the word is horizontal, false otherwise.
+         * @param word       Word to be placed.
+         * @param start      The coordinate where the word begins on the board.
+         * @param newLetters List of letters in word coming from the frame.
+         * @param blanks     List of letters in word which are blank tiles
+         */
+        Move(boolean horizontal, String word, Coordinate start, ArrayList<Integer> newLetters, ArrayList<Integer> blanks) {
+            this.horizontal = horizontal;
+            this.word = word;
+            this.start = start;
+            this.newLetters = newLetters;
+            this.blanks = blanks;
+        }
+
+        boolean isHorizontal() {
+            return horizontal;
+        }
+
+        String getWord() {
+            return word;
+        }
+
+        Coordinate getStartCoordinate() {
+            return start;
+        }
+
+        // Returns the list of integers corresponding to their position in the word.
+        ArrayList<Integer> getNewLetters() {
+            return newLetters;
+        }
+
+        //Returns the list of letters in word which are blank tiles
+        ArrayList<Integer> getBlanks() {
+            return blanks;
+        }
+
+        @Override
+        public String toString() {
+            return horizontal + " " + word + " " + start + " " + newLetters + blanks;
+        }
+
+    }
+
     // Build a GADDAG with dictionary words -----------------------------------------------------------------
     private static class GADDAG {
 
