29dba666611ee265e9f9a319456745bd066fabbe
rajitbanerjee <rajit.banerjee@ucdconnect.ie>
2020-05-22 19:51:30 +0100

    :sparkles: get best move from all possible moves

--- src/main/java/bot/DarkMode.java
+++ src/main/java/bot/DarkMode.java
@@ -1,10 +1,14 @@
 package bot;
 
-import game.Player;
+import game.*;
+import logic.Scoring;
 import logic.Scrabble;
+
+import java.util.List;
 
 /**
  * DarkMode is a Scrabble playing bot.
+ * // TODO javadoc comments
  *
  * @author Rajit Banerjee, 18202817
  * @author Tee Chee Guan, 18202044
@@ -12,15 +16,66 @@ import logic.Scrabble;
  * Team 15: DarkMode
  */
 public class DarkMode {
-    static Player player;
+    private static Player player;
+    private static Board board;
 
     public static void setPlayer(Player player) {
         DarkMode.player = player;
     }
 
-    public static String getCommand() {
+    public static String getCommand(Board board) {
+        DarkMode.board = board;
         Scrabble.printToOutput("Bot's frame: " + player.getFrame().getFrame().toString());
-        return "PASS";
+        return getBestMove();
+    }
+
+    public static Board getBoard() {
+        return board;
+    }
+
+    public static Frame getFrame() {
+        return player.getFrame();
+    }
+
+    // Gets a String of only the contents of the bot's frame
+    public static String getFrameLetters() {
+        StringBuilder letters = new StringBuilder();
+        for (Tile t : player.getFrame().getFrame()) {
+            char ch = t.getType();
+            if (Character.isLetter(ch) || ch == '_') {
+                letters.append(ch);
+            }
+        }
+        return letters.toString();
+    }
+
+    // Returns a String representation of the highest scoring word placement available
+    private static String getBestMove() {
+        board.storeHorizontalWords();
+        board.storeVerticalWords();
+        List<Word> moves = MoveGenerator.getAllMoves();
+        if (moves.size() == 0) {
+            String frame = getFrameLetters();
+            // Exchange some tiles at random if no suitable moves are found
+            int i1 = (int) (Math.random() * frame.length());
+            int i2 = (int) (Math.random() * frame.length());
+            if (i1 == i2) {
+                return "EXCHANGE " + frame.charAt(0);
+            } else {
+                return "EXCHANGE " + frame.substring(Math.min(i1, i2), Math.max(i1, i2));
+            }
+        } else {
+            Word best = moves.get(0);
+            int bestScore = Scoring.calculateScore(best, board);
+            for (Word move : moves) {
+                int score = Scoring.calculateScore(move, board);
+                if (score > bestScore) {
+                    best = move;
+                    bestScore = score;
+                }
+            }
+            return best.toString();
+        }
     }
 
 }
